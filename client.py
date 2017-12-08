692a4eb4a9e9d88045b7d0fc3f19f7c499f7f7bf
Sushant Mimani <sushant.mimani@gmail.com>
2017-12-08 17:47:32 -0500

    Reorganized imports

--- client.py
+++ client.py
@@ -1,17 +1,15 @@
-import socket, sys, time, binascii, os, pickle, getpass, ast, ConfigParser
-import threading
-import math
+import socket, sys, time, binascii, os, pickle, getpass, ast, ConfigParser, threading, math
 
 from select import select
 
-import signal
 
 from CryptoUtils import create_hash, keygen, generate_key_from_password, load_public_key, \
-                        symmetric_encryption, asymmetric_encryption, generate_password_hash, symmetric_decryption,\
-                        serialize_public_key, sign_message, serialize_private_key, get_diffie_hellman_params, generate_key_from_password_no_salt
+    symmetric_encryption, asymmetric_encryption, generate_password_hash, symmetric_decryption, \
+    serialize_public_key, sign_message, serialize_private_key, get_diffie_hellman_params, \
+    generate_key_from_password_no_salt
+
 
 class ChatClient:
-
     def __init__(self, port, ip, pub_key):
         self.exit_from_server_nonce = ""
         self.MAX_RETRIES = 3
@@ -85,7 +83,7 @@ class ChatClient:
             h2 = create_hash(challenge[1])
             answer = str(int(binascii.hexlify(h1), 16) & int(binascii.hexlify(h2), 16))
             nonce_1 = str(time.time())
-            message = answer + "\n"+self.username+"\n"+self.password_hash+"\n"+self.salt+"\n"+nonce_1
+            message = answer + "\n" + self.username + "\n" + self.password_hash + "\n" + self.salt + "\n" + nonce_1
             encrypted_message = asymmetric_encryption(self.server_pub_key, message)
             self.sock.send(encrypted_message)
             data = self.sock.recv(self.BUFFER_SIZE)
@@ -99,14 +97,14 @@ class ChatClient:
                 self.private_key, self.public_key = keygen()
                 serialize_private_key(self.private_key, self.username)
                 nonce_3 = str(time.time())
-                message = n2+"\n"+nonce_3+"\n"+serialize_public_key(self.public_key)
+                message = n2 + "\n" + nonce_3 + "\n" + serialize_public_key(self.public_key)
                 enc_msg, iv, tag = symmetric_encryption(self.derived_key, message)
                 payload = {
                     "message": str(enc_msg),
                     "iv": str(iv),
                     "tag": str(tag),
                     "client_port": self.recv_socket.getsockname()
-                    }
+                }
                 self.sock.send(pickle.dumps(payload))
                 data_1 = self.sock.recv(self.BUFFER_SIZE)
                 data = pickle.loads(data_1)
@@ -148,7 +146,8 @@ class ChatClient:
             self.sock.send(pickle.dumps(new_message))
             data_1 = self.sock.recv(self.BUFFER_SIZE)
             data = pickle.loads(data_1)
-            users, ni,ni1 = symmetric_decryption(self.derived_key,data["iv"], data["tag"],data["ciphertext"]).split("\n")
+            users, ni, ni1 = symmetric_decryption(self.derived_key, data["iv"], data["tag"], data["ciphertext"]).split(
+                "\n")
             temp = ast.literal_eval(users)
             user_list = []
             for x in temp:
@@ -157,8 +156,7 @@ class ChatClient:
             if len(user_list) == 0:
                 print "<-- No other users online"
             else:
-                print("<-- Signed In Users: {0}").format(','.join(user_list)) # Print user list
-
+                print("<-- Signed In Users: {0}").format(','.join(user_list))  # Print user list
 
     def is_user_address_available(self, user):
         final = self.sender_addresses.update(self.client_addresses)
@@ -173,7 +171,6 @@ class ChatClient:
                     return final[u]
         else:
             return False
-
 
     # This function handles the overall working of the client
     def start(self):
@@ -202,7 +199,8 @@ class ChatClient:
                         elif command_val == self.disconnect:
                             if len(input_array) != 2:
                                 print "incorrect usage. Correct usage disconnect <client_name>"
-                            if (input_array[1] not in self.client_shared_keys.keys()) or (not self.is_user_address_available(input_array[1])):
+                            if (input_array[1] not in self.client_shared_keys.keys()) or (
+                                    not self.is_user_address_available(input_array[1])):
                                 print "connection to " + input_array[1] + " not established yet."
                             else:
                                 self.perform_client_session_termination(input_array[1])
@@ -215,13 +213,14 @@ class ChatClient:
                                 self.message_for_client = " ".join(input_array[2:])
                                 user_address = self.is_user_address_available(chat_with_user)
                                 if (chat_with_user in self.dh_session_keys.keys()) and user_address:
-                                    self.send_message_to_client(self.message_for_client, self.dh_session_keys[chat_with_user], user_address)
+                                    self.send_message_to_client(self.message_for_client,
+                                                                self.dh_session_keys[chat_with_user], user_address)
                                 else:
                                     nonce_l = str(time.time())
                                     message = nonce_l
                                     ct, iv, tag = symmetric_encryption(self.derived_key, message)
                                     signature = sign_message(self.private_key, ct)
-                                    if(chat_with_user == self.username):
+                                    if (chat_with_user == self.username):
                                         print('Sender and receiver are the same.')
                                         continue
                                     new_message = {
@@ -237,7 +236,7 @@ class ChatClient:
 
                                     self.sock.send(pickle.dumps(new_message))
                                     # receive message from server with ticket-to-client etc
-                                    data= self.sock.recv(self.BUFFER_SIZE)  # buffer size is 65507 bytes
+                                    data = self.sock.recv(self.BUFFER_SIZE)  # buffer size is 65507 bytes
                                     if data == "null":
                                         print("User doesn't exist!")
                                     else:
@@ -251,14 +250,16 @@ class ChatClient:
                                         receiver_iv = temp["iv"]
                                         receiver_tag = temp["tag"]
                                         # decrypt response from server to get keys and ticket_to data for communicating to receiver
-                                        decrypted_response = symmetric_decryption(self.derived_key, iv1, tag1, server_response_totalk)
+                                        decrypted_response = symmetric_decryption(self.derived_key, iv1, tag1,
+                                                                                  server_response_totalk)
                                         decrypted_response_dict = pickle.loads(decrypted_response)
                                         # get the ticket_to_Receiver
                                         server_generated_shared_key = decrypted_response_dict["shared_key"]
                                         self.client_shared_keys[chat_with_user] = server_generated_shared_key
                                         receiver_address = decrypted_response_dict["receiver"][0]
                                         receiver_address_port = decrypted_response_dict["receiver"][1]
-                                        self.client_addresses[chat_with_user] = (receiver_address, receiver_address_port)
+                                        self.client_addresses[chat_with_user] = (
+                                            receiver_address, receiver_address_port)
 
                                         ticket_to_receiver = decrypted_response_dict["ticket_to"]
                                         payload = {
@@ -268,10 +269,11 @@ class ChatClient:
                                             "tag": receiver_tag
                                         }
 
-                                        self.send_socket.sendto(pickle.dumps(payload), (receiver_address, receiver_address_port))
+                                        self.send_socket.sendto(pickle.dumps(payload),
+                                                                (receiver_address, receiver_address_port))
 
                         else:
-                            if len(command_val) !=0:
+                            if len(command_val) != 0:
                                 print("Invalid Input!")
                                 print "Command list: list,send < username > < message > and exit. Have fun!"
 
@@ -296,7 +298,7 @@ class ChatClient:
         }
 
         self.sock.send(pickle.dumps(payload))
-        os.remove(self.username+"_private.pem")
+        os.remove(self.username + "_private.pem")
         os.remove(self.username + "_public.pem")
         self.sock.close()
         os._exit(0)
@@ -334,7 +336,8 @@ class ChatClient:
                         del_iv = data_dict["iv"]
                         del_tag = data_dict["tag"]
                         deleted_user = data_dict["user"]
-                        delete_confirm_nonce = symmetric_decryption(self.client_shared_keys[deleted_user], del_iv, del_tag, data_dict["data"])
+                        delete_confirm_nonce = symmetric_decryption(self.client_shared_keys[deleted_user], del_iv,
+                                                                    del_tag, data_dict["data"])
                         if self.terminate_nonce == float(delete_confirm_nonce) + 1:
                             print "terminated"
                             del self.client_shared_keys[deleted_user]
@@ -345,9 +348,11 @@ class ChatClient:
                         sender_user = data_dict["user"]
                         if sender_user in self.client_shared_keys.keys():
                             decrypted_msg_nonce = symmetric_decryption(self.client_shared_keys[sender_user],
-                                                                       data_dict["iv"], data_dict["tag"], data_dict["data"])
-                            response_nonce = float(decrypted_msg_nonce)+1
-                            encrypted_res, res_iv, res_tag = symmetric_encryption(self.client_shared_keys[sender_user], str(response_nonce))
+                                                                       data_dict["iv"], data_dict["tag"],
+                                                                       data_dict["data"])
+                            response_nonce = float(decrypted_msg_nonce) + 1
+                            encrypted_res, res_iv, res_tag = symmetric_encryption(self.client_shared_keys[sender_user],
+                                                                                  str(response_nonce))
                             terminate_valid_payload = {
                                 "message": "client_deleted",
                                 "data": encrypted_res,
@@ -355,7 +360,8 @@ class ChatClient:
                                 "tag": res_tag,
                                 "user": self.username
                             }
-                            self.send_socket.sendto(pickle.dumps(terminate_valid_payload), self.is_user_address_available(sender_user))
+                            self.send_socket.sendto(pickle.dumps(terminate_valid_payload),
+                                                    self.is_user_address_available(sender_user))
                             del self.client_shared_keys[data_dict["user"]]
                             del self.dh_session_keys[data_dict["user"]]
 
@@ -381,10 +387,11 @@ class ChatClient:
                         self.nonceN3 = str(time.time())
 
                         reply_message = {
-                                "N1": self.nonceN2,
-                                "N2": self.nonceN3
+                            "N1": self.nonceN2,
+                            "N2": self.nonceN3
                         }
-                        encrypted_response, nonce_iv, nonce_tag = symmetric_encryption(shared_key, pickle.dumps(reply_message))
+                        encrypted_response, nonce_iv, nonce_tag = symmetric_encryption(shared_key,
+                                                                                       pickle.dumps(reply_message))
                         # encrypted_response = "response"
 
                         payload = {
@@ -403,13 +410,13 @@ class ChatClient:
                         nonce_tag = data_dict["tag"]
                         nonce_user = data_dict["user"]
                         nonce_values = symmetric_decryption(self.client_shared_keys[nonce_user], nonce_iv, nonce_tag,
-                                             data_dict["encrypted_response"])
+                                                            data_dict["encrypted_response"])
                         nonce_values_dict = pickle.loads(nonce_values)
 
                         self.perform_diffie_hellman(data_dict["client"],
                                                     self.username,
                                                     nonce_values_dict["N2"])
-                    #     step1 is A->B with message (g^a mod p), B receives this message
+                    # step1 is A->B with message (g^a mod p), B receives this message
                     if message == "diffie-step1":
                         # This will be received by the receiver always. In case for A->B, B will receive this message
                         params = get_diffie_hellman_params()
@@ -422,13 +429,15 @@ class ChatClient:
                         diffie_msg_step1_iv = data_dict["iv"]
                         diffie_msg_step1_tag = data_dict["tag"]
                         # plaintext is g^a mod p, plaintest=x in DH algorithm
-                        plaintext_data = symmetric_decryption(self.client_shared_keys[sender_name], diffie_msg_step1_iv, diffie_msg_step1_tag, part)
+                        plaintext_data = symmetric_decryption(self.client_shared_keys[sender_name], diffie_msg_step1_iv,
+                                                              diffie_msg_step1_tag, part)
                         plaintext_data_dict = pickle.loads(plaintext_data)
                         x = plaintext_data_dict["x"]
                         if plaintext_data_dict["nonceN2"] == self.nonceN2:
-                        # computing the power(x, b) mod b part
+                            # computing the power(x, b) mod b part
                             session_key_val = math.pow(float(x), b) % p
-                            session_key = generate_key_from_password_no_salt(str((session_key_val * params["b"]) % params["p"]))
+                            session_key = generate_key_from_password_no_salt(
+                                str((session_key_val * params["b"]) % params["p"]))
                             self.dh_session_keys[sender_name] = session_key
 
                             # this is the creation of x = (g^b mod p) part from side B
@@ -460,7 +469,8 @@ class ChatClient:
                         diffie_msg_tag = data_dict["tag"]
                         part = data_dict["part"]
                         # the plaintext is (g^b mod p) part. This is y in diffie hellman algorithm
-                        plaintext = symmetric_decryption(self.client_shared_keys[sender_name], diffie_msg_iv, diffie_msg_tag, part)
+                        plaintext = symmetric_decryption(self.client_shared_keys[sender_name], diffie_msg_iv,
+                                                         diffie_msg_tag, part)
                         # computing the power(y, a) mod p part here.
                         session_key_val_sender = math.pow(float(plaintext), b) % p
 
@@ -470,7 +480,7 @@ class ChatClient:
                         # sender_name is the name of sender who sent this message
                         self.send_message_to_client(self.message_for_client, self.dh_session_keys[sender_name],
                                                     self.client_addresses[sender_name])
-                    #  this is message receiving part in diffie helman message exchange. B receives this message in
+                    # this is message receiving part in diffie helman message exchange. B receives this message in
                     # communication  A->B
                     if message == "chat_message":
                         receivd_data = data_dict["data"]
@@ -528,4 +538,3 @@ if __name__ == "__main__":
     server_pub_key = config.get('server_config', 'server_pub_key')
     cs = ChatClient(server_port, server_ip, server_pub_key)
     cs.login()
-
